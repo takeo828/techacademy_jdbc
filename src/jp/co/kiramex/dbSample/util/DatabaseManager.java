f2204cc59eaff16cef1d89e34e83fa34194fedb1
takeo828 <takeo.i828@gmail.com>
2023-02-13 01:57:52 +0900

    Lesson10 Chapter7 ここまでで作成したコードのコミット

---
+++ src/jp/co/kiramex/dbSample/util/DatabaseManager.java
@@ -0,0 +1,33 @@
+package jp.co.kiramex.dbSample.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DatabaseManager {
+    private static Connection con;
+
+    public static Connection getConnection() throws SQLException, ClassNotFoundException{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        con = DriverManager.getConnection(
+                "jdbc:mysql://localhost/world?useSSL=false&allowPublickeyRetrival=true",
+                "root",
+                "Xf875423"
+                );
+
+        return con;
+
+    }
+
+    public static void close() {
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
